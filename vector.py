f39dfd474f6ca426c1ee57b9a84ccb9d423ae439
Ishamu Joseph Sakibou <74374860+jishamu8@users.noreply.github.com>
2021-02-17 19:59:06 +0100

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,46 +1,4 @@
-'''
-Exercici 3: Feu un programa vectors() que mostri lo següent:
-
-
-************************
-* Programa per fer operacions amb vectors
-*
-
-Les opcions son:
-	1. Calcular el maxim d'un vector
-	2. Calcular la suma dels elements d'un vector
-	3. Sortir
-'''	
 #vectors.py
-def llegir_vector():
-	print("Dimensio del vector? ")
-	n = int(input())
-	v = []
-	for i in range(n):
-		print("Entra la component numero " + str(i+1) + ":")
-		x = int(input())
-		v.append(x)
-
-	return v
-
-
-
-def maxim(v):
-	n = len(v)
-	m = -1000
-	for i in range(n):
-			if v[i] > m:
-				m = v[i]
-
-	return m
-
-def suma(v):
-    n = len(v)
-    s = 0
-    for i in range(n):
-            s = s + v[i]
-    return s
-
 def capcalera():
         print("*******************************************")
         print("*Programa per fer operacions amb vectors")
@@ -64,29 +22,74 @@ def vectors():
                 vector = llegir_vector()
                 if opcio == 1:
                         m = maxim(vector)
-                        print("El maxim es " + str (m))
-                if opcio == 2: 
+                        print("El maxim es " + str(m))
+                elif opcio == 2: 
                         s = suma(vector)
-                        print("La suma es " + str (s))
+                        print("La suma es " + str(s))
 
                 opcio = menu()
         print("adeu")
 
 
+def llegir_vector():
+	print("Dimensio del vector? ")
+	n = int(input())
+	v = []
+	for i in range(n):
+		print("Entra la component numero " + str(i+1) + ":")
+		x = int(input())
+		v.append(x)
+
+	return v
+
+
+
+def maxim(v):
+	n = len(v)
+	m = -1000
+	p = -1
+	for i in range(1,n):
+		if v[i] > m:
+			m = v[i]
+			p = i 
+                 
+
+	return m,p
+
+def suma(v):
+	n = len(v)
+	s = 0
+	for i in range(n):
+		s = s + v[i]
+	return s
+
+def igual(g):
+  	if  g[0] == g[-1]:
+  		return "Si, primer i ultim son igual"
+  	else:
+  		return "No, primer i ultim son igual"
+
+def igual_2(g_2):
+  	if  g_2[0] == g_2[-1] and g_2[1] == g_2[-2]:
+  		return "Si, primer i ultim son igual, segon i penultim son iguals "
+  	else:
+  		return "No,  primer i ultim son igual, segon i penultim son iguals "
+
+
 
 def main():
-	vectors()
-
-
-
+#	vectors()
 	
-'''	vector = llegir_vector()
+	vector = llegir_vector()
 	print(vector)
-	max = maxim(vector)
-	print("la maxim dels volors es " + str(max))
-
+	max,pos = maxim(vector)
+    
+	print("la maxim dels volors es " + str(max) + " i la posicio es " +  str(pos))
 	sum = suma(vector)
 	print("La suma dels valors entrats es " + str(sum))
-
+	ret=igual(vector)
+	print (ret)
+	ret_2=igual_2(vector)
+	print (ret_2)
+	
 main()
-'''
