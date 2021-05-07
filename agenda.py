2eeaa5da95feb75b7a2bf5e7ab3bbef30b0404fd
jishamu <cf20ishamu.joseph@iesjoandaustria.org>
2021-05-07 16:41:31 +0200

    new

--- agenda.py
+++ agenda.py
@@ -1,130 +1,143 @@
 from contacte import Contacte
-def menu():
-        print("\n")
-        print("Les opcions desponibles son:\n")
-        print("    1. Afegir contacte\n")
-        print("    2. Consultar contacte\n")
-        print("    3. Esborrar contacte\n")
-        print("    4. Llistar contactes\n")
-        print("    5. Guardar contactes\n")
-        print("    6. Carregar contactes\n")
-        print("    7. Ordenar contactes\n")
-        print("    8. Sortir\n")
-        print("\n")
-        print("Entreu la vostra opcion:",end="")
-        o = int(input())
-        return o
 
 agenda = []
 
 def presenta():
-    print("************************************************")
-    print("*Programa per gestionar una agenda de contactes*")
-    print("************************************************")
+    print()
+    print("**************************************************")
+    print("* Programa per gestionar una agenda de contactes *")
+    print("**************************************************")
+    print()
 
 def numeric(x):
     n = len(x)
     for i in range(n):
         c = x[i]
         if c.isalpha():
-            return False
+           return False
+
     return True
 
-
 def alta():
+    
     n = input("Entra el nom: ")
-    cn = input("Entra el cognoms: ")
-    t = input("Entra el numero mobil: ")
+    cn= input("Entra els cognoms: ")
+    t = input("Entra el numero de mobil: ")
     while not numeric(t):
-        print("Error!!! Has d'escriure un valor numeric")
-        t=input("Entra el numero de mobil: ")
-
+        print("ERROR: Has d'escriure un valor numeric")
+        t = input("Entra el numero de mobil: ")
+        
     c = Contacte(n,cn,t)
     agenda.append(c)
 
-def llistar():
-    n = len(agenda)
-    for i in range (n):
-        c=agenda[i]
-        print(str(i+1) + ". " + c.toString())
-
 def consulta():
     llistar()
-    print("Aquest son el contacte possible")
-    q = int(input("Entra el numero de contacte a consultar: "))
+    print()
+    q = int(input("Entra el numero de contacte a consultar:"))
     c = agenda[q-1]
     c.mostrar()
+
 
 def esborrar():
     llistar()
     print()
-    q = int(input("Entra el numero de contacte a esborar: "))
+    q = int(input("Entra el numero de contacte a esborrar:"))
     agenda.pop(q-1)
 
-def guardar():
+    
+
+def llistar():
+    print()
+    print("Aquests son els contactes disponibles:")
+    print()
     n = len(agenda)
-    f = open("contactes.txt", "w")
+    for i in range(n):
+        c = agenda[i]
+        print(str(i+1) + ". " + c.toString())  
+
+def guardar():
+    # abans de guardar esborrem tot lo que hi hagi el fitxer
+    f = open("contactes.txt","w")
     f.close()
+
+    n = len(agenda)
     for i in range(n):
         c = agenda[i]
         c.guardar()
 
 def carregar():
-    f = open("contactes.txt", "r")
+    f = open("contactes.txt","r")
     c = Contacte("","","")
     i = 0
-
     while c.carregar(f):
-        agenda.append(c)
+        agenda.append(c) 
         c = Contacte("","","")
         i = i + 1
-    print("S'han carregat " + str(i)+ " contactes")
+        
+    print("S'han carregat " + str(i) + " contactes")
 
-
-def ordenar(v):
-    n = len(v)
+def ordenar(agenda):
+    n = len(agenda)
     for i in range(n):
             for j in range(n):
-                    if i<j:
-#                         si i<j es cierto quiere ecir que el elemento v[i]
-#                         es anterior a v[j] -> si además v[i] > v[j]
-                            if v[i] > v[j]:
-                                    aux = v[i]
-                                    v[i] = v[j]
-                                    v[j] = aux
-    return v
+                    if agenda[i].cognoms < agenda[j].cognoms:
+                            aux = agenda[i]
+                            agenda[i] = agenda[j]
+                            agenda[j] = aux
+    return agenda
+    
+
+def menu():
+    print()
+    print("Opcions disponibles:")
+    print(" 1. Afegir contacte")
+    print(" 2. Consultar contacte")
+    print(" 3. Esborrar contacte")
+    print(" 4. Llistar contactes")
+    print(" 5. Guardar contactes")
+    print(" 6. Carregar contactes")
+    print(" 7. Ordenar contactes")
+    print(" 8. Sortir")
+    print()
+    print("Entreu la vostra opcio:",end="")
+    o = int(input())
+    return o
+
 
 def main():
     carregar()
     presenta()
     o = menu()
-    while o !=8:
+    while o != 8:
         if o == 1:
             alta()
-        
+
         if o == 2:
             consulta()
 
         if o == 3:
             esborrar()
-                
+
         if o == 4:
             llistar()
-        
+
         if o == 5:
-           guardar()
-        
+            guardar()
+
         if o == 6:
-           carregar()
-        
+            carregar()
+
         if o == 7:
-           print(ordenar([3,4,5,1]))
-        
+            ordenar()
+            
         o = menu()
-    
+
     r = input("Vols salvar els nous contactes? [def. Si]")
-    if  len(r) == 0 or r[0] == "s" or  r[0] == "S":
+    if len(r) == 0 or r[0] == "S" or r[0] == "s":
         guardar()
-    print("Adeu")
+    print("Adeu!")
+
 
 main()
+
+
--- contacte.py
+++ contacte.py
@@ -1,36 +1,37 @@
 class Contacte:
-    def __init__(self,nom, cognoms, mobil):
+ 
+    def __init__(self,nom,cognoms,mobil):
         self.nom = nom
         self.cognoms = cognoms
         self.mobil = mobil
         self.mail = ""
-    
+ 
     def toString(self):
-        return self.nom + " "  + self.cognoms + " " + self.mobil
-
+        return self.nom + " " + self.cognoms + " " + self.mobil
+ 
     def mostrar(self):
+        print("______________________________________")
         print("Nom del contacte: " + self.nom)
         print("Cognoms del contacte: " + self.cognoms)
-        print("Mobil del contacte: " + self.mobil)
-        print("Mail del contacte: " + self.mail)
-    
+        print("Mobil del contacte: " + self.mobil )
+        print("Email del contacte: " + self.mail)
+ 
     def guardar(self):
-        f=open("contactes.txt", "a")
-        f.write(self.nom + "\n")
-        f.write(self.cognoms + "\n")
+        f = open("contactes.txt","a")
+        f.write(self.nom + "\n")        
+        f.write(self.cognoms + "\n")        
         f.write(self.mobil + "\n")
         f.write(self.mail + "\n")
-        f.close
-
-    def carregar(self, fitxer):
+        f.close()
+ 
+ 
+    def carregar(self,fitxer):
         aux = fitxer.readline().replace("\n","")
-        print ("aux = " + aux)
-        if len(aux) > 0 :
+        if len(aux) > 0:
             self.nom = aux
             self.cognoms = fitxer.readline().replace("\n","")
             self.mobil = fitxer.readline().replace("\n","")
             self.mail = fitxer.readline().replace("\n","")
-        
-
             return True
-        return False
+ 
+        return False        
